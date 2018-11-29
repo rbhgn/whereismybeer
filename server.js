3a11dbefc6ee037c34fdf78aa8bfe9b758e48936
rbhgn <robhgn@gmail.com>
2018-11-29 11:42:16 +0100

    Fixing COR Again

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 var cors = require('cors')
 
-app.use(express.static(path.join(__dirname, 'build')));
-app.use(cors());
+
+// app.use(cors())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+app.use( express.static(path.join(__dirname, 'build')) )
+
 app.get('*', function (req, res) {
  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
