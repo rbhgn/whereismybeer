d224a96ee402818dddee6e3166ef9c641d5c04ca
rbhgn <robhgn@gmail.com>
2018-11-29 11:27:23 +0100

    Fixed Cors

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
+var cors = require('cors')
 
 app.use(express.static(path.join(__dirname, 'build')));
-
+app.use(cors());
 app.get('*', function (req, res) {
  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
