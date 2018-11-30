27883feb6e196e6de4f59b4fbfad050ea8c97341
rbhgn <robhgn@gmail.com>
2018-11-30 09:57:07 +0100

    Fixed top Spacing

--- src/App.js
+++ src/App.js
@@ -167,7 +167,7 @@ const styles = ({
   wrapperSide: {
     width: '25%',
     margin: '10px',
-    paddingTop: '60px',
+    paddingTop: '70px',
     overflow: 'hidden',
     zIndex: 10,
     position: 'relative'
@@ -175,7 +175,7 @@ const styles = ({
   wrapperMiddle: {
     width: '40%',
     margin: '10px',
-    paddingTop: '60px',
+    paddingTop: '70px',
     overflow: 'hidden',
     zIndex: 10,
     position: 'relative'
